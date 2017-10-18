52f87805fc34b4d37455a8ebb8b7820ba9a5386b
Matt Glover <matthewglover@me.com>
2017-10-18 09:22:07 +0100

    Refactor DefaultRouterBuilder
    - Add: addHandlers to RequestRouter to add multiple handlers in one hit

--- src/main/java/com/matthewglover/request_handler/RequestRouter.java
+++ src/main/java/com/matthewglover/request_handler/RequestRouter.java
@@ -6,10 +6,17 @@ import com.matthewglover.http_response.HttpResponseFactory;
 import com.matthewglover.http_response.HttpResponseTemplate;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class RequestRouter {
 
     private final ArrayList<RequestHandler> handlers = new ArrayList<>();
+
+    public void addHandlers(List<RequestHandler> handlers) {
+        for (RequestHandler handler : handlers) {
+            addHandler(handler);
+        }
+    }
 
     public void addHandler(RequestHandler handler) {
         handlers.add(handler);
